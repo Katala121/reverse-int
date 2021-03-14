b5ce433f84376747aae8a0a90c450e45a360463a
Katala121 <sanyafootb@list.ru>
2021-03-14 04:26:25 +0300

    feat: implement fanction

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) return Number(String(n).split('').slice(1).reverse().join(''))
+  return Number(String(n).split('').reverse().join(''))
 }
